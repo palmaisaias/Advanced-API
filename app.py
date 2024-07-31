aaf6d560a3a835105ab2ee5e1d3b66f0f7688f7e
palmaisaias <palmaisaias@yahoo.com>
2024-07-30 20:25:08 -0700

    Swagger Docs intro set up. No routes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from database import db
 from models.schemas import ma
 from limiter import limiter
 from caching import cache
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from models.customer import Customer
 from models.order import Order
@@ -14,6 +15,12 @@ from routes.customer_routes import customer_bp
 from routes.product_routes import product_bp
 from routes.order_routes import order_bp
 from routes.cart_routes import cart_bp
+
+#Swagger setup section
+SWAGGER_URL = '/api/docs' #url endpoint for the docs
+API_URL = '/static/swagger.yaml'
+
+swagger_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': 'Shopping API'})
 
 def create_app(config_name):
 
@@ -32,6 +39,7 @@ def blueprint_config(app):
     app.register_blueprint(product_bp, url_prefix='/products')
     app.register_blueprint(order_bp, url_prefix='/orders')
     app.register_blueprint(cart_bp, url_prefix='/cart')
+    app.register_blueprint(swagger_blueprint, url_prefix=SWAGGER_URL)
 
 def rate_limit_config():
     limiter.limit("200 per day")(customer_bp)
